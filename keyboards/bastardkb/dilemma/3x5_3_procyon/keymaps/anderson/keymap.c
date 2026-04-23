d3f9e4cd7881049ca9cce681dc9e8173242b06d1
quanchobi <quanchobi@proton.me>
2026-04-22 21:30:51 -0400

    change if to switch

--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/anderson/keymap.c
+++ keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/anderson/keymap.c
@@ -360,9 +360,9 @@ bool oled_task_user(void) {
 
     oled_set_cursor(0, 15);
     led_t led_state = host_keyboard_led_state();
-    if (led_state.num_lock) oled_write_P(PSTR("NUM "), false);
-    if (led_state.caps_lock) oled_write_P(PSTR("CAPS "), false);
-    if (led_state.scroll_lock) oled_write_P(PSTR("SCR "), false);
+    oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
+    oled_write_P(led_state.caps_lock ? PSTR("CAPS ") : PSTR("     "), false);
+    oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
 
 #    ifdef WPM_ENABLE
     oled_set_cursor(14, 15);
